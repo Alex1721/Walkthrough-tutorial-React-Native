de3cd5a176a197a47125a999de0e1c1305ca8d56
alexandre <alexandre.boving@gmail.com>
2023-12-04 20:46:13 +0100

    Fifth commit
    The gesture functionality is finished.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ In this tutorial the following topics will be covered:
 - Break down the app layout and implement it with flexbox.
 - Use each platform's system UI to select an image from the media library.
 - Create a sticker modal using the "<Modal>" and "<FlatList>" components from React Native.
-- Add touch gestures to interact with a sticker.
+- Add touch gestures to interact with a sticker. We implement the gestures from React Native Gesture Handler and Reanimated libraries.
 - Use third-party libraries to capture a screenshot and save it to the disk.
 - Handle platform differences between Android, iOS, and web.
 - Finally, go through the process of configuring a status bar, a splash screen, and an icon to complete the app.
--- myApp/components/EmojiSticker.js
+++ myApp/components/EmojiSticker.js
@@ -1,13 +1,66 @@
-import { View, Image } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated"; // Looks at the style prop of the component. It also determines which values to animate and applies updates to create an animation.
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0); // Attention que l'ordre à son importance.
+
+  const scaleImage = useSharedValue(imageSize);
+  /**
+   * It mutates a piece of data and allows running animations based on the current value.
+   * A shared value can be accessed and modified using the .value property.
+   * It will scale the initial value of scaleImage so that when a user-double taps the sticker, it scales to twice its original size.
+   * To do this, we will create an object and call it doubleTap.
+   */
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
